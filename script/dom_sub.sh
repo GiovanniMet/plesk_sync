c37934cd793977c5d9751ce4a8f8705d01e21232
Giovanni Metitieri <GiovanniMet@users.noreply.github.com>
2016-01-04 18:50:08 +0100

    add

---
+++ script/dom_sub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+line_to_domain_subdomain(){
+	#create one line for each domain and subdomain
+	echo "Generating entries..."
+	for domain in `mysql psa -u admin -p$(cat /etc/psa/.psa.shadow) -Ns -e 'select name from domains' | sort -u`; do
+  		domip=`/usr/local/psa/bin/domain --info $domain | grep address | cut -d\: -f2 | sed -e 's/^[ \t]*//'`;
+  		if [ $domip ]; then
+   			echo $domip  $domain www.$domain >> lista_host;
+  		fi
+	done
+	echo "Done!"
+}
+line_to_domain_subdomain
