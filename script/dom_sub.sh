f841871f6442f335d2dd8198358eaac91b632c3f
Giovanni Metitieri <GiovanniMet@users.noreply.github.com>
2016-01-05 09:10:00 +0100

    fix

--- script/dom_sub.sh
+++ script/dom_sub.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 line_to_domain_subdomain(){
-	#create one line for each domain and subdomain
-	echo "Generating entries..."
-	echo "vi /etc/hosts" > lista_host
-	for domain in `mysql psa -u admin -p$(cat /etc/psa/.psa.shadow) -Ns -e 'select name from domains' | sort -u`; do
-  		domip=`/usr/local/psa/bin/domain --info $domain | grep address | cut -d\: -f2 | sed -e 's/^[ \t]*//'`;
-  		if [ $domip ]; then
-   			echo "$domip  $domain www.$domain" >> lista_host;
-  		fi
-	done
-	echo "Done!"
+        #create one line for each domain and subdomain
+        echo "Generating entries..."
+        echo "vi /etc/hosts" > lista_host
+        for domain in `mysql psa -u admin -p$(cat /etc/psa/.psa.shadow) -Ns -e 'select name from domains' | sort -u`; do
+                domip=`/usr/local/psa/bin/domain --info $domain | grep address | cut -d\: -f2 | sed -e 's/^[ \t]*//'`;
+                if [ $domip ]; then
+                        echo "$domip  $domain www.$domain" >> lista_host;
+                fi
+        done
+        echo "Done!"
 }
 
 line_to_domain_subdomain
