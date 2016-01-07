f537b038966a924c05c7d05906045a21c0c09b24
Giovanni Metitieri <GiovanniMet@users.noreply.github.com>
2016-01-07 18:40:01 +0100

    add

---
+++ script/set_ip_it.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+main(){
+        #create one line for each domain and subdomain
+        echo "Generating entries..."
+        echo "#!/bin/bash" > update_ip.sh
+        for domain in `mysql psa -u admin -p$(cat /etc/psa/.psa.shadow) -Ns -e 'select name from domains' | sort -u`; do
+                domip=`/usr/local/psa/bin/domain --info $domain |  grep -i 'Indirizzo IP:' | cut -d\: -f2 | sed -e 's/^[ \t]*//'`;
+                if [ $domip ]; then
+                        echo "/usr/local/psa/bin/domain --update domain -ip$domip" >> update_ip.sh;
+                fi
+        done
+        echo "Done!"
+}
+
+main
