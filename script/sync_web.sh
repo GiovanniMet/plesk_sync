d3656776565c5dcf69a49cae8e1e9c24f4bad1c5
Giovanni Metitieri <GiovanniMet@users.noreply.github.com>
2016-03-13 01:54:59 +0100

    update

--- script/sync_web.sh
+++ script/sync_web.sh
@@ -12,20 +12,23 @@ sync_web(){
         DOMAIN=`echo ${each} | cut -d. -f2,3`; #domain.ext
         SUBDOMAIN=`echo ${each} | cut -d. -f1`; #subdomain
         echo "A: $each D: $DOMAIN , S: $SUBDOMAIN "
-        if [ `ssh -q -l$TARGET_USER -p$TARGET_PORT $TARGET "ls /var/www/vhosts/ | grep ^$each"` ]; then
-            echo -e "${purple}Syncing data for ${white}$each${purple}...${noclr}"
+		if [ "`mysql -u admin -p$(cat /etc/psa/.psa.shadow) -Ns psa -e "select htype from domains where name = '$each';"`" ==  "std_fwd" ]; then
+            echo -e "$each is a redirect..."
+        elif [ `ssh -q -l$TARGET_USER -p$TARGET_PORT $TARGET "ls /var/www/vhosts/ | grep ^$each"` ]; then
+            echo -e "Syncing data for $each..."
             rsync -avHPe "ssh -q -p$TARGET_PORT" /var/www/vhosts/$each $TARGET_USER@$TARGET:/var/www/vhosts/ --exclude=conf --update >> web_sync.log 2>&1
             rsync -avHPe "ssh -q -p$TARGET_PORT" /var/www/vhosts/$each/httpdocs $TARGET_USER@$TARGET:/var/www/vhosts/$each/ --update >> web_sync.log 2>&1
             rsync -avHPe "ssh -q -p$TARGET_PORT" /var/www/vhosts/$each/httpsdocs $TARGET_USER@$TARGET:/var/www/vhosts/$each/ --update >> web_sync.log 2>&1
-        elif [ `mysql -u admin -p$(cat /etc/psa/.psa.shadow) -Ns psa -e "select htype from domains where name = '$each';"` ==  "std_fwd" ]; then #check Redirect
-            echo -e "${purple}$each ${white}is a redirect{purple}...${noclr}"
         elif [ `ssh -q -l$TARGET_USER -p$TARGET_PORT $TARGET "ls /var/www/vhosts/$DOMAIN/subdomains/ | grep ^$SUBDOMAIN$"` ]; then
-            echo -e "${purple}Syncing data for ${white}$each${purple}...${noclr}"
+            echo -e "Syncing data for $each..."
             rsync -avHPe "ssh -q -p$TARGET_PORT" /var/www/vhosts/$DOMAIN/subdomains/$SUBDOMAIN/ $TARGET_USER@$TARGET:/var/www/vhosts/$DOMAIN/subdomains/$SUBDOMAIN/ --exclude=conf --update >> web_sync.log 2>&1
             rsync -avHPe "ssh -q -p$TARGET_PORT" /var/www/vhosts/$DOMAIN/subdomains/$SUBDOMAIN/httpdocs $TARGET_USER@$TARGET:/var/www/vhosts/$DOMAIN/subdomains/$SUBDOMAIN/ --update >> web_sync.log 2>&1
             rsync -avHPe "ssh -q -p$TARGET_PORT" /var/www/vhosts/$DOMAIN/subdomains/$SUBDOMAIN/httpsdocs $TARGET_USER@$TARGET:/var/www/vhosts/$DOMAIN/subdomains/$SUBDOMAIN/ --update >> web_sync.log 2>&1
+		elif [ "ls /var/www/vhosts/$DOMAIN/$SUBDOMAIN/" ] && [ `ssh -q -l$TARGET_USER -p$TARGET_PORT $TARGET "ls /var/www/vhosts/$DOMAIN/$SUBDOMAIN/"` ]; then
+            echo "Syncing data for $each..."
+            rsync -avHPe "ssh -q -p$TARGET_PORT" /var/www/vhosts/$DOMAIN/$SUBDOMAIN/ $TARGET_USER@$TARGET:/var/www/vhosts/$DOMAIN/$SUBDOMAIN/ --exclude=conf --update >> web_sync.log 2>&1
         else
-            echo -e "${red}$each did not restore remotely${noclr}"
+            echo -e "$each did not restore remotely"
             echo -e $each >> web_sync.err
        fi
     done
