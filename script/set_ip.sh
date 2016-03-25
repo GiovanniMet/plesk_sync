507c3c6f396b952452c3208f4338c1e93b7ede1a
Giovanni Metitieri <GiovanniMet@users.noreply.github.com>
2016-03-25 09:03:22 +0100

    fix

--- script/set_ip.sh
+++ script/set_ip.sh
@@ -8,7 +8,7 @@ main(){
         for domain in `mysql psa -u admin -p$(cat /etc/psa/.psa.shadow) -Ns -e 'select name from domains' | sort -u`; do
                 domip=`/usr/local/psa/bin/domain --info $domain | grep address | cut -d\: -f2 | sed -e 's/^[ \t]*//'`;
                 if [ $domip ]; then
-                        echo "/usr/local/psa/bin/domain --update domain -ip$domip" >> update_ip.sh;
+                        echo "/usr/local/psa/bin/domain --update $domain -ip$domip" >> update_ip.sh;
                 fi
         done
         echo "Done!"
